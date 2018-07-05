d7ec8cdd5d2f26951e3102ef6e3b830e7f86303d
pulkit <pulkit.komal6@gmail.com>
2018-07-05 19:48:20 +0530

    July 2 2018

--- Fifth Week/6.py
+++ Fifth Week/6.py
@@ -1,30 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
 
 
 response = requests.get("https://gaana.com/artist/marshmello")
 soup = BeautifulSoup(response.text,"html.parser")
-
 #print(soup.prettify())
 titlelist = []
 durList = []
+
+
+
+
+
 tdTags = soup.find_all('span', class_="parentnode sourcelist_1256640")
 for tag in tdTags:
     #print(tag.text)
     result = re.findall(r"\w+", tag.text)
-    #print('-----------')
-    z = result.index('duration')
     y = result.index('title')
     x = result.index('id')
-    dur = result[z+1]
-    durList.append(dur)
-    #print(dur)
-    u = result[y+1:x]
+    u = result[y + 1:x]
     title = ' '.join(u)
     titlelist.append(title)
-    #print(title)
 
+    #print('-----------')
+    z = result.index('duration')
+
+    dur = result[z+1]
+    s=int(dur)
+    durList.append(s)
+    #print(dur)
+
+    #print(title)
+dataset = list(zip(titlelist,durList))
+df = pd.DataFrame(data=dataset,columns=["title","duration"])
+df.to_csv("abc.csv",index=False,header=True)
 print(titlelist)
 print(durList)
 
+plt.bar(titlelist, durList, label="Duration", width=0.5)
+
+plt.legend()
+
+plt.xlabel("Title")
+plt.ylabel("Duration")
+
+plt.title("Data")
+
+plt.show()
