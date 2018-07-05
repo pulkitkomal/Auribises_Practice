6b2936d0aec4185f4f9dadd04a920ebd5721b9fa
pulkit <pulkit.komal6@gmail.com>
2018-07-05 20:42:41 +0530

    July 5 2018

--- Fifth Week/9.py
+++ Fifth Week/9.py
@@ -5,65 +5,71 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
-
-response = requests.get("https://gaana.com/artist/marshmello")
-soup = BeautifulSoup(response.text,"html.parser")
-
 durList = []
-
-tdTags = soup.find_all('span', class_="parentnode sourcelist_1256640")
-for tag in tdTags:
-
-    result = re.findall(r"\w+", tag.text)
-    z = result.index('duration')
-    dur = result[z+1]
-    s=int(dur)
-    durList.append(s)
-
-response = requests.get('https://gaana.com/artist/arijit-singh')
-soup = BeautifulSoup(response.text,"html.parser")
-
 durListAJ = []
 
-CHTags = soup.find_all('span', class_="parentnode sourcelist_52767")
-for tag in CHTags:
-
-    result = re.findall(r"\w+", tag.text)
-    z = result.index('duration')
-    dur = result[z+1]
-    s=int(dur)
-    durListAJ.append(s)
+def mm():
+    response = requests.get("https://gaana.com/artist/marshmello")
+    soup = BeautifulSoup(response.text,"html.parser")
 
 
 
-dataset = list(zip(durList,durListAJ))
-df = pd.DataFrame(data=dataset,columns=["Marshmello","Arijit Singh"])
-df.to_csv("maj.csv",index=False,header=True)
+    tdTags = soup.find_all('span', class_="parentnode sourcelist_1256640")
+    for tag in tdTags:
 
-data = pd.read_csv("maj.csv")
+        result = re.findall(r"\w+", tag.text)
+        z = result.index('duration')
+        dur = result[z+1]
+        s=int(dur)
+        durList.append(s)
+def aj():
+    response = requests.get('https://gaana.com/artist/arijit-singh')
+    soup = BeautifulSoup(response.text,"html.parser")
 
-X = data["Marshmello"].values
-Y = data["Arijit Singh"].values
 
-data = stats.linregress(X,Y)
-b1 = data[0]
-b0 = data[1]
+    CHTags = soup.find_all('span', class_="parentnode sourcelist_52767")
+    for tag in CHTags:
 
-print("b1 is",b1)
-print("b0 is",b0)
+        result = re.findall(r"\w+", tag.text)
+        z = result.index('duration')
+        dur = result[z+1]
+        s=int(dur)
+        durListAJ.append(s)
 
-Y1 = []
+def dataOut():
+    dataset = list(zip(durList,durListAJ))
+    df = pd.DataFrame(data=dataset,columns=["Marshmello","Arijit Singh"])
+    df.to_csv("maj.csv",index=False,header=True)
 
-for x in X:
-    y = b0 + (b1*x)
-    Y1.append(y)
+def plotGraph():
+    data = pd.read_csv("maj.csv")
+    X = data["Marshmello"].values
+    Y = data["Arijit Singh"].values
 
-print("============")
-print(Y1)
-print("============")
+    data = stats.linregress(X,Y)
+    b1 = data[0]
+    b0 = data[1]
 
-plt.xlabel('Marshmello')
-plt.ylabel('Arijit Singh')
-plt.grid(True)
-plt.plot(X, Y, "o", X, Y1)
-plt.show()
+    print("b1 is",b1)
+    print("b0 is",b0)
+
+    Y1 = []
+
+    for x in X:
+        y = b0 + (b1*x)
+        Y1.append(y)
+
+    print("============")
+    print(Y1)
+    print("============")
+
+    plt.xlabel('Marshmello')
+    plt.ylabel('Arijit Singh')
+    plt.grid(True)
+    plt.plot(X, Y, "o", X, Y1)
+    plt.show()
+
+mm()
+aj()
+dataOut()
+plotGraph()
