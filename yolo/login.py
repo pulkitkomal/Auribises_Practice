525d8a5eaeb5b726fc3ec786fc8af89ac9577035
pulkit <pulkit.komal6@gmail.com>
2018-08-01 22:13:14 +0530

    1 August

--- yolo/login.py
+++ yolo/login.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
-
 driver = webdriver.Chrome(chrome_options=options, executable_path=r'/home/pulkit/Videos/chromedriver')
+
 data = []
 data_tr = []
 
@@ -11,13 +11,16 @@ def site_login():
     user = input('Enter Roll No.: ')
     passwrd = input('Enter Password: ')
     URL = 'https://academics.gndec.ac.in/'
+
     driver.get(URL)
     driver.find_element_by_id('username').send_keys('{}'.format(user))
     driver.find_element_by_id ('password').send_keys('{}'.format(passwrd))
+
     python_button = driver.find_elements_by_xpath("/html/body/div/div[2]/div/div/form/button")[0]
     python_button.click()
     python_button1 = driver.find_elements_by_xpath("/html/body/div/div[2]/div/div/div[2]/form/button")[0]
     python_button1.click()
+
     a = driver.find_elements_by_tag_name('tr')
     for x in a:
         data_tr.append(x.text)
@@ -25,11 +28,10 @@ def site_login():
     for x in b:
         data.append(x.text)
 
-    driver.quit()
-
 
 
 def attendenceDATA():
+    print()
     for x in range(0, 8):
         print(data_tr[x])
     y = 9
