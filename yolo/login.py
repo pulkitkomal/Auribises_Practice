b65f39c94a92be110ac1ca25da83d7e5aa11199e
pulkit <pulkit.komal6@gmail.com>
2018-08-01 21:29:06 +0530

    1 August

--- yolo/login.py
+++ yolo/login.py
@@ -7,10 +7,12 @@ driver = webdriver.Chrome(chrome_options=options, executable_path=r'/home/pulkit
 data = []
 data_tr = []
 def site_login():
+    user = input('Enter Roll No.: ')
+    passwrd = input('Enter Password: ')
     URL = 'https://academics.gndec.ac.in/'
     driver.get(URL)
-    driver.find_element_by_id('username').send_keys('1615368')
-    driver.find_element_by_id ('password').send_keys('anon@1234')
+    driver.find_element_by_id('username').send_keys('{}'.format(user))
+    driver.find_element_by_id ('password').send_keys('{}'.format(passwrd))
     python_button = driver.find_elements_by_xpath("/html/body/div/div[2]/div/div/form/button")[0]
     python_button.click()
     python_button1 = driver.find_elements_by_xpath("/html/body/div/div[2]/div/div/div[2]/form/button")[0]
@@ -25,48 +27,57 @@ def site_login():
     driver.quit()
 
 
-site_login()
-for x in range(0,8):
-    print(data_tr[x])
 
-def lecNeeded(p, q):
-    print("lec needed called")
-    p=p+1
-    q=q+1
-    print(p,q)
-    perc2=(q/p)*100
-    print(perc2)
-    lecNeed = q - needLECT
-    print(lecNeed)
-    return lecNeed
+def attendenceDATA():
+    for x in range(0, 8):
+        print(data_tr[x])
+    y = 9
+    z = 10
+    totallect = 11
+    lectureatt = 12
+    j = 13
+    booll = True
+    um = 0
 
-
-y = 9
-z = 10
-totallect= 11
-lectureatt = 12
-j = 13
-
-try:
     for v in range(0,15):
         print()
+        if um == 1:
+            booll = True
         perc = float(data[j])
         totLECT= float(data[totallect])
-        needLECT = float(data[lectureatt])
-        print(data[y],data[z],'\nTotal Lectures: ',totLECT,'\nLectures Attented: ',needLECT,'\nPercentage:', perc,'%')
+        attLECT = float(data[lectureatt])
+        print(data[y],data[z],'\nTotal Lectures: ',totLECT,'\nLectures Attented: ',attLECT,'\nPercentage:', perc,'%')
         y += 6
         z += 6
         j += 6
         totallect += 6
         lectureatt += 6
         perc2=perc
+        p = totLECT
+        q = attLECT
+        needLECT = 0
         if(perc2<75.00):
-            p = totLECT
-            q = needLECT
-            res = lecNeeded(p, q)
+            while(booll):
+                p = p + 1
+                q = q + 1
+                perc3 = (q / p) * 100
+                if perc3 >= 75.00:
+                    booll = False
+                    um = 1
+                lecNeed = q - int(data[12])
+                needLECT= lecNeed
+        print('Lectures you need to attend for 75%: ',needLECT)
 
-        print(res)
 
+
+
+
+
+
+
+try:
+    site_login()
+    attendenceDATA()
 
 except:
     pass
